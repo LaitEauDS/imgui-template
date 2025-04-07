cb53a328d95f332c30a683aa02d8f0bf078a31e1
Léo De Santis <leo.desantispro@gmail.com>
2025-04-07 22:36:11 +0200

    :construction: ...

--- src/ChessBoard.cpp
+++ src/ChessBoard.cpp
@@ -1,5 +1,9 @@
 #include "ChessBoard.hpp"
+#include "Bishop.hpp"
+#include "King.hpp"
+#include "Knight.hpp"
 #include "Pawn.hpp"
+#include "Queen.hpp"
 #include "Rook.hpp"
 #include <imgui.h>
 #include <cstddef>
@@ -8,15 +12,37 @@
 
 ChessBoard::ChessBoard() : m_selectedPieceIndex(-1)
 {
+    //Pions
     for (int i = 8; i < 16; i++)
-        m_board[i] = std::make_unique<Pawn>(Color::White);
-    for (int i = 48; i < 56; i++)
         m_board[i] = std::make_unique<Pawn>(Color::Black);
+    for (int i = 48; i < 56; i++)
+        m_board[i] = std::make_unique<Pawn>(Color::White);
 
-    m_board[0] = std::make_unique<Rook>(Color::White);
-    m_board[7] = std::make_unique<Rook>(Color::White);
-    m_board[56] = std::make_unique<Rook>(Color::Black);
-    m_board[63] = std::make_unique<Rook>(Color::Black);
+    //Tours
+    m_board[0] = std::make_unique<Rook>(Color::Black);
+    m_board[7] = std::make_unique<Rook>(Color::Black);
+    m_board[56] = std::make_unique<Rook>(Color::White);
+    m_board[63] = std::make_unique<Rook>(Color::White);
+
+    //Cavaliers
+    m_board[1] = std::make_unique<Knight>(Color::Black);
+    m_board[6] = std::make_unique<Knight>(Color::Black);
+    m_board[57] = std::make_unique<Knight>(Color::White);
+    m_board[62] = std::make_unique<Knight>(Color::White);
+
+    // Fous
+    m_board[2] = std::make_unique<Bishop>(Color::Black);
+    m_board[5] = std::make_unique<Bishop>(Color::Black);
+    m_board[58] = std::make_unique<Bishop>(Color::White);
+    m_board[61] = std::make_unique<Bishop>(Color::White);
+
+    // Dames
+    m_board[3] = std::make_unique<Queen>(Color::Black);
+    m_board[59] = std::make_unique<Queen>(Color::White);
+
+    // Rois
+    m_board[4] = std::make_unique<King>(Color::Black);
+    m_board[60] = std::make_unique<King>(Color::White);
 }
 
 void ChessBoard::display_board()
@@ -32,17 +58,17 @@ void ChessBoard::display_board()
         boardPtrs[i] = m_board[i] ? m_board[i].get() : nullptr;
     }
 
-    std::vector<int> possibleMoves;
-    if (m_selectedPieceIndex != -1 && m_board[m_selectedPieceIndex]) {
-        possibleMoves = m_board[m_selectedPieceIndex]->get_possible_moves(m_selectedPieceIndex, boardPtrs);
-    }
-
     for (int x = 0; x < 8; x++)
     {
         for (int y = 0; y < 8; y++)
         {
             int index = x * 8 + y;
             ImGui::PushID(index);
+
+            std::vector<int> possibleMoves;
+            if (m_selectedPieceIndex != -1 && m_board[m_selectedPieceIndex]) {
+                possibleMoves = m_board[m_selectedPieceIndex]->get_possible_moves(m_selectedPieceIndex, boardPtrs);
+            }
 
             ImVec4 backgroundColor = ((x + y) % 2 == 0) ? whiteColor : blackColor;
 
@@ -62,16 +88,26 @@ void ChessBoard::display_board()
             if (m_board[index])
             {
                 switch (m_board[index]->get_type()) {
-                    case PieceType::Pawn: 
+                    case PieceType::Pawn:
                         label = (m_board[index]->get_color() == Color::White) ? "p" : "o";
-                        textColor = (m_board[index]->get_color() == Color::White) ? ImVec4(1, 1, 1, 1) : ImVec4(0, 0, 0, 1);
                         break;
-                    case PieceType::Rook: 
+                    case PieceType::Rook:
                         label = (m_board[index]->get_color() == Color::White) ? "r" : "t";
-                        textColor = (m_board[index]->get_color() == Color::White) ? ImVec4(1, 1, 1, 1) : ImVec4(0, 0, 0, 1);
                         break;
-                    default: 
-                        label = ""; 
+                    case PieceType::Knight:
+                        label = (m_board[index]->get_color() == Color::White) ? "n" : "m";
+                        break;
+                    case PieceType::Bishop:
+                        label = (m_board[index]->get_color() == Color::White) ? "b" : "v";
+                        break;
+                    case PieceType::Queen:
+                        label = (m_board[index]->get_color() == Color::White) ? "q" : "w";
+                        break;
+                    case PieceType::King:
+                        label = (m_board[index]->get_color() == Color::White) ? "k" : "l";
+                        break;
+                    default:
+                        label = "";
                         break;
                 }
             }
@@ -82,12 +118,17 @@ void ChessBoard::display_board()
             }
 
             if (ImGui::Button(label.c_str(), ImVec2{70.f, 70.f})) {
-                if (m_board[index] && m_board[index]->get_color() == Color::White) {
+                if (m_board[index] && m_board[index]->get_color() == m_current_color) {
                     m_selectedPieceIndex = index;
                 }
-                else if (m_selectedPieceIndex != -1 && std::find(possibleMoves.begin(), possibleMoves.end(), index) != possibleMoves.end()) {
+                else if (
+                    m_selectedPieceIndex != -1 &&
+                    std::find(possibleMoves.begin(), possibleMoves.end(), index) != possibleMoves.end()
+                ) {
                     m_board[index] = std::move(m_board[m_selectedPieceIndex]);
                     m_selectedPieceIndex = -1;
+                
+                    m_current_color = (m_current_color == Color::White) ? Color::Black : Color::White;
                 }
             }
 
